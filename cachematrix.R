235dcf18194b35f3e05d3ccf842f0f6317fcbf00
dkurkowski <dkurkowski@users.noreply.github.com>
2014-11-23 11:11:39 -0600

    Update cachematrix.R
    Defined changes by DKurkowski for R Programming course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# Requirement states: This function creates a special "matrix" object that can 
+# cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  NewInv <- NULL
+  SetNew <- function(y) {
+## Introduce new concept called super assignment
+    x <<- y
+    NewInv <<- NULL
+  }
+  get <- function() x
+  SetInv <- function(inverse) NewInv <<- inverse
+  GetInv <- function() NewInv
+  list(SetNew = SetNew, get = get, SetInv = SetInv, GetInv = GetInv)
 }
 
-
-## Write a short comment describing this function
+# Requirement states: This function computes the inverse of the special "matrix" 
+# returned by makeCacheMatrix above. If the inverse has already been calculated 
+# (and the matrix has not changed), then cacheSolve should retrieve the inverse 
+# from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  NewInv <- x$GetInv()
+##Returns the inverse if it has been calculated along with a user defined message
+  if (!is.null(NewInv)) {
+    message("Data cached as expected")
+    return(NewInv)
+  }
+##Calculate the inverse. Done for the first run of our testing scenario
+  data <- x$get()
+  NewInv <- solve(data, ...)
+  x$SetInv(NewInv)
+  NewInv
 }
